740b37f7fe7fd49dcacde7c8e0b44fa1e19ceff8
Katarzyna Kasprzak <kasia.kasprzak18@gmail.com>
2016-09-08 10:45:32 +0200

    fix: proper url_path and collection

--- lib/show_more/view_helpers.rb
+++ lib/show_more/view_helpers.rb
@@ -1,12 +1,9 @@
 module ShowMore
   module ViewHelpers
-    def show_more_button(partial)
-      collection = @class_name.class.name.pluralize.downcase
-      new_path = request.original_fullpath.to_s + "?all=true"
-      link_to 'Show More', url_for(new_path, {
-                              all: true,
-                              collection: collection,
-                              partial: partial}),
+    def show_more_button partial
+      collection = @class_name.name.pluralize.downcase
+      new_path = request.original_fullpath.to_s + "?all=true&collection=#{collection}&partial=#{partial}"
+      link_to 'Show More', url_for(new_path),
                             remote: :true,
                             id: "#{partial}_show_more"
     end
